796e6307831a9e638b191c49356e9a2bda6703c9
kq4hy <kq4hy@virginia.edu>
2015-10-30 23:03:07 -0400

    Initial Rewards Implementation

--- 4710 HW3 - ticketToRide/src/ttr/model/player/Skylar.java
+++ 4710 HW3 - ticketToRide/src/ttr/model/player/Skylar.java
@@ -1,9 +1,11 @@
 package ttr.model.player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 import ttr.model.destinationCards.*;
@@ -16,9 +18,13 @@ public class Skylar extends Player {
 	public State curr_state;
 	public Routes routesList = Routes.getInstance();
 	public ArrayList<Route> allRoutes = routesList.getAllRoutes();
+	public HashMap<TrainCardColor, Integer> current_train_cards = new HashMap<TrainCardColor, Integer>();
 	public HashMap<Route, Integer> routes_to_claim = new HashMap<Route, Integer>();
 	//public ArrayList<City> Cities = new ArrayList<City>();
 	public HashMap<Destination,City> CitiesMap = new HashMap<Destination,City>();
+	public List<TrainCardColor> color_list = Arrays.asList(TrainCardColor.rainbow, TrainCardColor.black, 
+			TrainCardColor.blue, TrainCardColor.green, TrainCardColor.orange, TrainCardColor.purple, 
+			TrainCardColor.red, TrainCardColor.white, TrainCardColor.yellow); 
 	
 	public Skylar(String name) {
 		super(name);
@@ -35,20 +41,139 @@ public class Skylar extends Player {
 	public void makeMove() {
 		routesList = Routes.getInstance();
 		allRoutes = routesList.getAllRoutes();
+		compute_hand();
 		update_graph();
 		ArrayList<DestinationTicket> curr_destinations = this.getDestinationTickets();
 		for(DestinationTicket ticket: curr_destinations) {
-			computePaths(CitiesMap.get(ticket.getFrom()), this.CitiesMap);
-			System.out.println("Distance from " + CitiesMap.get(ticket.getFrom()) + " to " + CitiesMap.get(ticket.getTo()) + ": " + CitiesMap.get(ticket.getTo()).minDistance);
-			List<Route> list_routes = getShortestPathTo(CitiesMap.get(ticket.getTo()));
+			ArrayList<Route> list_routes = new ArrayList<Route>();
+			list_routes = getShortestPath(ticket.getFrom(), ticket.getTo());
 		    System.out.println("Path: " + list_routes);
 			for(Route r: list_routes){
 				if(routes_to_claim.get(r) == null)
 					routes_to_claim.put(r, 0);
 				routes_to_claim.put(r, routes_to_claim.get(r) + r.getPoints());
 			}
+			CitiesMap.clear();
+			initiateGraph();
+		}
+		
+		if(curr_state == unbuy_able) {
+			// calculate reward and then move
+			int maximum_reward = 0;
+			for(Action a: curr_state.get_actions()) {
+				if(a.name.equals("a3")) {
+					// a3: amount of points that the player receives from claiming a route after drawing a train color card
+					// calculate the probability of getting a certain color
+					HashMap<TrainCardColor, Integer> temp_hand = new HashMap<TrainCardColor, Integer>();
+					double total_reward = 0;
+					for(TrainCardColor curr_color: color_list) {
+						temp_hand.put(curr_color, temp_hand.get(curr_color) + 1);
+						ArrayList<Route> buyable_routes = get_buyable_routes(routes_to_claim, temp_hand);
+						int maximum_points = 0;
+						if(buyable_routes.size() !=  0) {
+							for(Route r : buyable_routes) {
+								if(routes_to_claim.get(r) > maximum_points)
+									maximum_points = routes_to_claim.get(r);
+							}
+						}
+						total_reward += maximum_points / 9; 
+					}
+					a.set_reward((int)total_reward);
+					if((int) total_reward > maximum_reward)
+						maximum_reward = (int)total_reward;
+					System.out.println("a3 reward is: " + (int)total_reward);
+				} if(a.name.equals("a4")) {
+					// a4: amount of points that you can get if you save up a specific color instead of buying immediately					
+					// just return the maximum number of points AI can get if saving up
+					int curr_maxim = 0;
+					for(Map.Entry<Route, Integer> entry : routes_to_claim.entrySet()){ 
+						if(entry.getValue() > curr_maxim)
+							curr_maxim = entry.getValue();
+					} 
+					a.set_reward(curr_maxim);
+					if(curr_maxim > maximum_reward)
+						maximum_reward = curr_maxim;
+					System.out.println("a4 reward is: " + curr_maxim);
+				} if(a.name.equals("a5")) {
+					// a5: 25 - sum of current points from the destination cards	
+					if(curr_destinations.size() > 4 || this.getTotalDestTicketCost() >= 25)
+						a.set_reward(0);
+					else
+						a.set_reward(25 - this.getTotalDestTicketCost());
+					if(a.get_reward() > maximum_reward)
+						maximum_reward = a.get_reward();	
+					System.out.println("a5 reward is: " + a.get_reward());
+				}
+			}
+		}
+		else if(curr_state == buy_able){
+			// calculate reward and then move
+			ArrayList<Route> buyable_routes = get_buyable_routes(routes_to_claim, current_train_cards);
+			Route maximum_route;
+			int maximum_reward = 0;
+			for(Action a: curr_state.get_actions()) {
+				if (a.name.equals("a1")) {
+					HashMap<TrainCardColor, Integer> temp_hand = current_train_cards;
+					int count = 0;
+					int sum = 0;
+					while(count != temp_hand.size() - 1) {
+						count = 0;
+						for(Route buy_route: buyable_routes) {
+							int cost = buy_route.getCost();
+							TrainCardColor color_needed = buy_route.getColor();
+							if(temp_hand.get(color_needed) <= cost) {
+								sum += routes_to_claim.get(buy_route);
+								temp_hand.put(color_needed, temp_hand.get(color_needed) - cost);
+							} else
+								count++;
+						}
+					}
+					a.set_reward(sum);
+					if(sum > maximum_reward) 
+						maximum_reward = sum;
+					System.out.println("a1 reward is: " + sum);
+					// a1: the number of points that the player gets for claiming that route as well as all subsequent routes that it can claim
+					
+				} if(a.name.equals("a2")) {
+					// a2: number of points that the player can receive for claiming that single route and then going back to the sate of un-buy-able
+					int maxi = 0;
+					for(Route buy_route: buyable_routes) {
+						if(routes_to_claim.get(buy_route) > maxi) {
+							maxi = routes_to_claim.get(buy_route);
+							maximum_route = buy_route;
+						}
+					}
+					a.set_reward(maxi);
+					if(maxi > maximum_reward) 
+						maximum_reward = maxi;
+					System.out.println("a2 reward is: " + maxi);
+				}
+			}
+			
 		}
 		super.drawTrainCard(0);
+	}
+	
+	// organizes a HashMap in accordance with how many train cards and of what color
+	public void compute_hand() { 
+		for(TrainCardColor a: color_list) {
+			if(current_train_cards.get(a) == null)
+				current_train_cards.put(a, 0);
+			current_train_cards.put(a, this.getNumTrainCardsByColor(a));
+		}
+	}
+	
+	// returns all the routes player can buy in the current routes to claim
+	public ArrayList<Route> get_buyable_routes(HashMap<Route, Integer> curr_routes, HashMap<TrainCardColor, Integer> curr_hand) {
+		ArrayList<Route> ret_route = new ArrayList<Route>();
+		for(Map.Entry<Route, Integer> entry : curr_routes.entrySet()){ 
+			TrainCardColor color_req = entry.getKey().getColor();
+			int cost = entry.getKey().getCost();
+			int num_trains = curr_hand.get(color_req);
+			if(cost <= num_trains) 
+				ret_route.add(entry.getKey());
+		}
+		return ret_route;
 	}
 	
 	public void initiate_actions(State buy, State cant_buy) {
@@ -178,7 +303,7 @@ public class Skylar extends Player {
 	    }
 	}
 	
-	public static void computePaths(City source, HashMap<Destination,City> cities)
+	public void computePaths(City source, HashMap<Destination,City> cities)
 	{
 	    source.minDistance = 0;
 	    PriorityQueue<City> vertexQueue = new PriorityQueue<City>();
@@ -203,8 +328,8 @@ public class Skylar extends Player {
 	    }
 	}
 	
-	public static List<Route> getShortestPathTo(City target) {
-		List<Route> routes = new ArrayList<Route>();
+	public ArrayList<Route> getShortestPathTo(City target) {
+		ArrayList<Route> routes = new ArrayList<Route>();
 
 		Route temp;
 	    for (City vertex = target; vertex != null; vertex = vertex.previous) {
@@ -221,13 +346,13 @@ public class Skylar extends Player {
 	//Takes in two Destinations and calculates the shortest path from start to dest
 	//Doesn't work while tested in this main b/c Routes.getInstance() doesn't work in this main,
 	//   but should work when run in TTRMain (not tested but test it for me!)
-	public List<Route> getShortestPath(Destination start, Destination dest) {
+	public ArrayList<Route> getShortestPath(Destination start, Destination dest) {
 		//this.CitiesMap.clear(); 
 		//this.initiateGraph();
 		this.update_graph();
 		this.computePaths(this.CitiesMap.get(start), this.CitiesMap);
 		
-		List<Route> routes = new ArrayList<Route>();
+		ArrayList<Route> routes = new ArrayList<Route>();
 
 		Route temp;
 	    for (City vertex = this.CitiesMap.get(dest); vertex != null; vertex = vertex.previous) {
@@ -246,13 +371,12 @@ public class Skylar extends Player {
 		public final String name;
 		public int reward = 0;
 		public ArrayList<Action> actions = new ArrayList<Action>();
-		public State(String n) {
-			name = n;
-		}
-		public void add_action(Action a) {
-			actions.add(a);
-		}
 		
+		public State(String n) { name = n; }
+		public void add_action(Action a) { actions.add(a); }
+		public ArrayList<Action> get_actions() {
+			return actions;
+		}
 	}
 	
 	static class Action {
@@ -268,22 +392,24 @@ public class Skylar extends Player {
 			currState = curr_state;
 			nextState = next_state;
 		}
+		public int get_reward() { return reward; }
+		public void set_reward(int i) { reward = i; }
 	}
 	
-	public static void main(String[] args) {
-		Skylar testSkylar = new Skylar();
-		testSkylar.initiateGraph();
-		for (City c: testSkylar.CitiesMap.values())
-			c.exfoliate();
-		
-		computePaths(testSkylar.CitiesMap.get(Destination.LosAngeles), testSkylar.CitiesMap);
-		for (City v : testSkylar.CitiesMap.values())
-		{
-		    System.out.println("Distance to " + v + ": " + v.minDistance);
-		    List<Route> routes = getShortestPathTo(v);
-		    System.out.println("Path: " + routes);
-		}
-		//testSkylar.getShortestPath(Destination.LosAngeles, Destination.Chicago);
-		
-	}
+//	public static void main(String[] args) {
+//		Skylar testSkylar = new Skylar();
+//		testSkylar.initiateGraph();
+//		for (City c: testSkylar.CitiesMap.values())
+//			c.exfoliate();
+//		
+//		computePaths(testSkylar.CitiesMap.get(Destination.LosAngeles), testSkylar.CitiesMap);
+//		for (City v : testSkylar.CitiesMap.values())
+//		{
+//		    System.out.println("Distance to " + v + ": " + v.minDistance);
+//		    List<Route> routes = getShortestPathTo(v);
+//		    System.out.println("Path: " + routes);
+//		}
+//		//testSkylar.getShortestPath(Destination.LosAngeles, Destination.Chicago);
+//		
+//	}
 }
