f7ac85ac05d661f9c1a6f91a8c4c67a8874a1e63
Josh Wang <jw6dz@virginia.edu>
2015-11-22 23:56:08 -0500

    updated lowest_enemy_desires to sort correctly, now ready to implement multi-negotiation stage

--- 4710 HW4 - Negotiator/Negotiator/negotiator.py
+++ 4710 HW4 - Negotiator/Negotiator/negotiator.py
@@ -41,7 +41,7 @@ class jw6dz(BaseNegotiator):
     total_util = 0
     acceptance_rate = 0.8
     num_elements = 0
-    turn_number = 1
+    turn_number = 0
     did_opponent_raise = False  # did the opponent demand more this turn than the last turn?
     curr_opponent_util = None   # current opponent perceived utility
     opponent_util_list = []     # list of opponent utilities based on their offers continuous from multiple negotiations
@@ -51,7 +51,15 @@ class jw6dz(BaseNegotiator):
 
         # if we're initiating the offer
         if offer is None:
+            # initiate to 1's if not initiated
+            if len(self.enemy_desires) == 0:
+                self.total_util = self.compute_total_util()
+                for s in self.preferences:
+                    self.enemy_desires[s] = (1, self.total_util/self.num_elements)
+
             self.offer = self.compute_offer()
+            print "THIS IS MY RETURN OFFER (when we initiate) turn:" + str(self.turn_number)
+            print self.offer
             return self.offer
 
         # initiate to 1's if not initiated
@@ -69,16 +77,20 @@ class jw6dz(BaseNegotiator):
                 num_occurences += temp_tuple[0]
             calc_point = float(self.total_util) / float(num_occurences)
             for s in self.enemy_desires:
-                self.enemy_desires[s] = (self.enemy_desires.get(s)[0], self.enemy_desires.get(s)[0]*calc_point)
+                self.enemy_desires[s] = (self.enemy_desires.get(s)[0], self.enemy_desires.get(s)[0] * calc_point)
         print "ENEMY DESIRES"
         print self.enemy_desires
 
+        # if it's the last turn, 50% (TBD) chance to reduce acceptance rate to 30%
+        if self.iter_limit == self.turn_number:
+            if random() < .5:
+                self.acceptance_rate = .3
 
-
+        # calculate the goal utility we want to reach with this offer
         goal_util = self.acceptance_rate * self.total_util
         enemy_desires_copy = self.enemy_desires.copy()
         lowest_enemy_desires = [] # list of tuples of (name, util)
-        while len(enemy_desires_copy) > 0:
+        while len(enemy_desires_copy) > 0:  # Complete lowest_enemy_desires list
             lowest_util = float(1000)
             lowest_name = ""
             lowest_occu = 0
@@ -88,15 +100,44 @@ class jw6dz(BaseNegotiator):
                     lowest_util = float(v[1])
                     lowest_occu = v[0]
             enemy_desires_copy.pop(lowest_name)
-            lowest_enemy_desires.append((lowest_name, lowest_occu, lowest_util))
-        print "LOWETS ENEMY DSFASDFKJLA"
+            lowest_enemy_desires.append((lowest_name, lowest_occu, lowest_util, self.preferences.get(lowest_name)))
+        print "LOWEST ENEMY DESIRES"
         print lowest_enemy_desires
         print "min utility we want this turn: " + str(goal_util)
+
+        # after completion, sort lowest_enemy_desires such that same util's are sorted by our preferences G->L
+        iter_count = 0
+        inside_count = 1
+        new_sorted_list = []
+        while iter_count < len(lowest_enemy_desires):
+            temp_list = []
+            initialized = False
+            while inside_count < len(lowest_enemy_desires):
+                if not initialized:
+                    temp_list.append(lowest_enemy_desires[inside_count-1])
+                    initialized = True
+                else:
+                    if lowest_enemy_desires[inside_count][2] != lowest_enemy_desires[inside_count-1][2]:
+                        inside_count += 1
+                        break
+                    else:
+                        temp_list.append(lowest_enemy_desires[inside_count])
+                        inside_count += 1
+
+            temp_list = sorted(temp_list, key=operator.itemgetter(3), reverse=True) # sort temp_list G->L before adding to new_sorted_list
+            for t in temp_list:
+                new_sorted_list.append(t)
+            iter_count = inside_count
+        print "NEW SORTED LIST WITH 4-TUPLE"
+        print new_sorted_list
+
+        # update lowest_enemy_desires with finalized sorted list
+        lowest_enemy_desires = new_sorted_list
 
         temp_return_offer = []
         current_turn_util = 0
         count = 0
-        while current_turn_util < goal_util:
+        while current_turn_util <= goal_util and count < len(lowest_enemy_desires):
             # print self.preferences.get(lowest_enemy_desires[count][0])
             # print lowest_enemy_desires[count][2]
             current_turn_util += self.preferences.get(lowest_enemy_desires[count][0])
@@ -109,26 +150,10 @@ class jw6dz(BaseNegotiator):
         self.acceptance_rate -= .1
         self.turn_number += 1
         return self.offer
-        # self.offer = offer
-
-        # if random() < 0.2 and offer is not None:
-        #     # Very important - we save the offer we're going to return as self.offer
-        #     print "I, jw6dz, agree that you can take " + str(self.offer)  ### IMPORTANT remove computing ID later
-        #     self.offer = BaseNegotiator.set_diff(self)
-        #     print "I, jw6dz, will take: " + str(self.offer)
-        #     return self.offer
-        # else:
-        #     ordering = self.preferences
-        #     ourOffer = []
-        #     for item in ordering.keys():
-        #         if random() < .5:
-        #             ourOffer = ourOffer + [item]
-        #     self.offer = ourOffer
-        #     return self.offer
 
     def compute_offer(self):
         self.turn_number += 1
-        tempList = self.preferences
+        tempList = self.preferences.copy()
         sortedList = sorted(tempList.items(), key=operator.itemgetter(1))
         sortedList.reverse()
         returnList = []
@@ -172,4 +197,6 @@ class jw6dz(BaseNegotiator):
         # Store the results of the last series of negotiation (points won, success, etc.)
     def receive_results(self, results):
         self.acceptance_rate = 0.8  # reset acceptance rate
-        self.turn_number = 1
+        self.turn_number = 0
+        # IDEAS:
+        # - if the opponent accepts the offer on the first turn (the initial offer), up the aceptance rate to 0.9 at the very least.
